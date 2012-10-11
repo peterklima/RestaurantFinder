accc5cbc048eb9d2e742fa036bf60ed6d386c781
Peter Klima <peter.klima@fh-hagenberg.at>
2012-10-11 16:33:19 +0200

    set up project new

--- README.md
+++
@@ -1,4 +0,0 @@
-RestaurantFinder
-================
-
-im620
--- src/at/fhooe/im620/restaurantfinder/dao/HibernateRestaurantDAOImpl.java
+++
@@ -1,8 +0,0 @@
-package at.fhooe.im620.restaurantfinder.dao;
-
-import at.fhooe.im620.restaurantfinder.bo.Restaurant;
-
-
-public class HibernateRestaurantDAOImpl extends GenericDAOImpl<Restaurant> {
-
-}
